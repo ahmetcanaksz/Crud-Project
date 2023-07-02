b437bbf904f3c9913fe425f4d6a73ea25dd0bd49
Emre Arıkboga <aksozcan3016@gmail.com>
2023-07-02 22:55:33 +0300

    The Crud Project

---
+++ CRUD-project/crud-project/src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+{
+    id:"1",
+    name:"Manoj Deshwal",
+    Age:"30",
+},{
+    id:"2",
+    name:"Manoj Deshwal",
+    Age:"32",
+},{
+    id:"3",
+    name:"Manoj Deshwal",
+    Age:"34",
+},
+]
+
+export default Employees;
